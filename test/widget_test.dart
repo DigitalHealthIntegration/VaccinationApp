c68eba3af7ec7f0c9591a1706c92d0785b10d461
mnandi-29 <mnandi@iprdgroup.com>
2021-02-02 13:17:05 +0530

    Unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 //import 'package:flutter_r1/main.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_r1/controllers/qr_utils.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -25,5 +26,15 @@ void main() {
     // // Verify that our counter has incremented.
     // expect(find.text('0'), findsNothing);
     // expect(find.text('1'), findsOneWidget);
+
+    String couponString = "healthpass:SHA256\ruBip0i3d6aovFPuBPocSpurs7vXfedVjPjBhCuSP9NIHGwkt3L1LQWEHuFpx2sFQhFdu31npoLQgkHnRi51aF/DlYLAABZRBnFesWtyI+UA2kYQCFoMOR3qs1Sn6XisF5ukI/rwI5mOn0t5w+VhmCUhxtMhXKmfe1MM8uWX/po=@vitorpamplona.com/vaccine-certificate-qrcode-generator/pub_key?type=coupon&phase=123&conditions=abc";
+    Map decodeMap = QrUtils.getInfoFromQR(couponString);
+    expect(decodeMap.toString(), "{type: coupon, phase: 123, conditions: abc}");
+
+    String couponString2 = "healthpass:SHA256\KvjzblZPVd6HQugVLd9vS3nRqAyKmOxcnUm2VfSvo9eJGRYV2hP1mYgnUM481pHBXLtUI0wYGX3DsmSHumxHQe+IMOdVWP/FTarJIJCN67YtRP6r0FMxfucbxINQOgxs5QWi+i0E1xToPsA0/EaL0FA2HFL6FMdv4i/JgRUGd/Y=@vitorpamplona.com/vaccine-certificate-qrcode-generator/pub_key?type=coupon&age=a%20b";
+    Map decodeMap2 = QrUtils.getInfoFromQR(couponString2);
+    print(decodeMap2);
+    expect(decodeMap2.toString(), "{type: coupon, age: a%20b}");
+
   });
 }
